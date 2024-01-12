efee807bebc534307473757264648f137e8ed465
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-01-12 21:22:33 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sveltekit + TailwindCSS + Daisyui + Typescript Template
+
+## Link to docs:
+
+- [Sveltekit](https://kit.svelte.dev/docs)
+- [TailwindCSS](https://tailwindcss.com/docs)
+- [DaisyUI](https://daisyui.com/docs/installation)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="flex h-screen w-full flex-col items-center justify-center gap-6">
+  <p class="text-4xl">Hello world!</p>
+  <div>
+    <h2 class="w-full text-center text-2xl">Docs:</h2>
+    <ul class="mt-4 flex list-none gap-2 [&_a]:btn [&_a]:btn-info">
+      <li><a href="https://kit.svelte.dev">Svelte Kit</a></li>
+      <li><a href="https://tailwindcss.com/">Tailwind CSS</a></li>
+      <li><a href="https://daisyui.com/">DaisyUI</a></li>
+    </ul>
+  </div>
+</div>
